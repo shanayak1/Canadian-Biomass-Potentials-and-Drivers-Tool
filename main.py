b6778b6595a9a1f2f59212dd7ae1289332d3b3e7
shanayak1 <shanayakailey@gmail.com>
2026-06-01 23:39:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
 
+def main_menu():
+    print("Please select a province to analyze by inputting the corresponding number:\n1. Alberta\n2. British Columbia\n")
+    user_province = input(">> ")
+    print(user_province)
+
+    if user_province == "1" or user_province == "2":
+        return user_province
+    else:
+        return 0
+    
+def options_menu():
+    print("Please select a biomass category to display by inputting the corresponding number:\n")
+    print("\t1. Forestry Biomass\n\t2. Agricultural Biomass\n\t3. Livestock Residue\n\t4. Urban Waste")
+    user_first_choice = input(">> ")
+
+    if user_first_choice == "1" or user_first_choice == "2" or user_first_choice == "3" or user_first_choice == "4":
+        return user_first_choice
+    else:
+        return 1
 
 print("Welcome to the Biomass Potentials and Drivers Tool!\n")
-user_province = input("Please select a province to analyze:\n1. Alberta\n2. British Columbia\n")
+
+while True:
+    user_province = main_menu()
+    
+    while user_province == 0:
+        print("Invalid input, please try again.")
+        user_province = main_menu()
+    print(user_province)
+
+    user_choice_one = options_menu()
+
+    while user_choice_one == 1:
+        print("Invalid Input, please try again")
+        user_choice_one = options_menu()
+    
+    print ("your choice is")
+    print(user_choice_one)
