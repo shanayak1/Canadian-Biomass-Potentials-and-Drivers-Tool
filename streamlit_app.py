eeb9d7dabb92e0492e7eb8101f93f2c09049945d
shanayak1 <shanayakailey@gmail.com>
2026-06-21 20:17:20 -0600

    yahoo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,33 @@
 import streamlit as st
 
-st.title("Ahoy!")
+#page setup
+home_page = st.Page(
+    page="pages/home.py",
+    title="Intro to Biomass",
+    icon=":material/account_circle:",
+    default = True,
+)
+
+potential_page = st.Page(
+    page="pages/potential.py",
+    title = "Biomass Potentials",
+    icon = ":material/bar_chart:",
+)
+
+#navigation menu
+
+pg = st.navigation(pages=[home_page, potential_page])
+
+# navigation with sections
+pg = st.navigation(
+    {
+        "Info": [home_page],
+        "projects": [potential_page]
+    }
+)
+
+
+#run navigation
+
+pg.run()
+
