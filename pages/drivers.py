5233c3108342d586a787f245b7a382a942ac1217
shanayak1 <shanayakailey@gmail.com>
2026-07-26 14:20:05 -0600

    Update drivers.py

--- pages/drivers.py
+++ pages/drivers.py
@@ -8,11 +8,24 @@ st.set_page_config(page_title = "Biomass Potentials and Availibility Drivers",
                    layout="wide")
 
 st.header('Biomass Potential and Availibility Drivers')
-st.subheader('Key Drivers')
+st.subheader('Key Drivers:')
 
+#create rows using a container
 with st.container(border=True):
     st.write("Forestry Biomass and Residues")
 with st.container(border=True):
     st.write("Agricultural Biomass and Residues")
 with st.container(border=True):
     st.write("Livestock Residues")   
+
+#create columns
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    if st.button("Crop Yield"):
+        st.session_state.selected_driver = "Crop Yield"
+with col2:
+    if st.button("City Population"):
+        st.session_state.selected_driver = "City Population"
+
+
