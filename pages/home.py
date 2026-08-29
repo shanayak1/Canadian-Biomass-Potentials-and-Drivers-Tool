bdea57897a3cbe7eb11ec9f0f35e4aba71fb1fdb
shanayak1 <shanayakailey@gmail.com>
2026-08-29 12:19:39 -0600

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -7,10 +7,6 @@ st.subheader("A Brief Introduction and Methodology")
 
 st.text("This tool has been created to build a better understanding of current biomass resources available in parts of Canada, as well as how these resource quantities will change in the future. To understand the key socio-economic and environmental drivers impacting biomass potential, the PESTEL (Political, Economic, Social, Technological, Environmental, Legal) framework. This tool will bring awareness to the current quantities of biomass resource within the provinces chosen in the scope of this project, detailing the energy potential available by province. This data helps to inform comparisions between how different provinces contribute to the greater biomass supply across the country, as well as discuss the factors that impact that biomass supply.")
 
-st.subheader("Terminology")
-
-st.text("The following terminology is used frequently throughout the biomass potentials and drivers tool:")
-
 definitions_df = pd.DataFrame({
     "Term": [
         "Bioeconomy",
@@ -26,7 +22,7 @@ definitions_df = pd.DataFrame({
 
 with st.container():
     st.subheader("Terminology")
-    
+    st.text("The following terminology is used frequently throughout the biomass potentials and drivers tool:")
     st.dataframe(
         definitions_df,
         hide_index = True,
